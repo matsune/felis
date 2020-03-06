8db452037c4ee6448294d84918f065b2b33dd388
Yuma Matsune <yuma.matsune@gmail.com>
2020-03-06 17:17:47 +0800

    added lexer test

--- test/parse/lexer_test.cc
+++ test/parse/lexer_test.cc
@@ -4,7 +4,7 @@
 #define NEXT(KIND)                \
   t = lexer.Next();               \
   ASSERT_FALSE(lexer.HasError()); \
-  ASSERT_EQ(t->kind, KIND);
+  ASSERT_EQ(t->kind, KIND)
 
 namespace felis {
 
@@ -13,79 +13,130 @@ TEST(LexerTest, EmptyInput) {
   in << "";
   Lexer lexer(in);
   auto t = std::make_unique<Token>();
-  NEXT(TokenKind::END)
+  NEXT(TokenKind::END);
 }
 
-TEST(LexerTest, lex) {
+TEST(LexerTest, Valid_Ident) {
   std::stringstream in;
-  in << "abcあ 12_21 23.1 true false 'a' \"string\"";
-  in << "fn let var ret ext if else ";
-  in << "+ - / * % & | ^ << >> && || < <= > >= == != !(){}=;:,-> /* comment */";
+  in << "abc あいう aa_あ ";
   Lexer lexer(in);
   std::unique_ptr<Token> t;
 
-  NEXT(TokenKind::IDENT)
-  ASSERT_EQ(t->sval, "abcあ");
-  ASSERT_FALSE(t->ws);
-  ASSERT_FALSE(t->nl);
+  NEXT(TokenKind::IDENT);
+  ASSERT_EQ(t->sval, "abc");
+  NEXT(TokenKind::IDENT);
+  ASSERT_EQ(t->sval, "あいう");
+  NEXT(TokenKind::IDENT);
+  ASSERT_EQ(t->sval, "aa_あ");
+}
 
-  NEXT(TokenKind::LIT_INT)
-  ASSERT_EQ(t->ival, 1221);
-  ASSERT_TRUE(t->ws);
-  ASSERT_FALSE(t->nl);
+TEST(LexerTest, Valid_Int) {
+  std::stringstream in;
+  in << "0 23_4__5 0b11 0o71";
+  Lexer lexer(in);
+  std::unique_ptr<Token> t;
 
-  NEXT(TokenKind::LIT_FLOAT)
-  ASSERT_EQ(t->fval, 23.1);
+  NEXT(TokenKind::LIT_INT);
+  ASSERT_EQ(t->ival, 0);
+  NEXT(TokenKind::LIT_INT);
+  ASSERT_EQ(t->ival, 2345);
+  NEXT(TokenKind::LIT_INT);
+  ASSERT_EQ(t->ival, 3);
+  NEXT(TokenKind::LIT_INT);
+  ASSERT_EQ(t->ival, 57);
+}
 
-  NEXT(TokenKind::LIT_BOOL)
-  ASSERT_EQ(t->bval, true);
+TEST(LexerTest, Valid_Float) {
+  std::stringstream in;
+  in << "0.27_2 3e-2 8e4";
+  Lexer lexer(in);
+  std::unique_ptr<Token> t;
 
-  NEXT(TokenKind::LIT_BOOL)
-  ASSERT_EQ(t->bval, false);
+  NEXT(TokenKind::LIT_FLOAT);
+  ASSERT_EQ(t->fval, 0.272);
+  NEXT(TokenKind::LIT_FLOAT);
+  ASSERT_EQ(t->fval, 3e-2);
+  NEXT(TokenKind::LIT_FLOAT);
+  ASSERT_EQ(t->fval, 8e4);
+}
 
-  NEXT(TokenKind::LIT_CHAR)
+TEST(LexerTest, Valid_Char) {
+  std::stringstream in;
+  in << "'a' '\\'' '\\\\'";
+  Lexer lexer(in);
+  std::unique_ptr<Token> t;
+
+  NEXT(TokenKind::LIT_CHAR);
   ASSERT_EQ(t->cval, 'a');
+  NEXT(TokenKind::LIT_CHAR);
+  ASSERT_EQ(t->cval, '\'');
+  NEXT(TokenKind::LIT_CHAR);
+  ASSERT_EQ(t->cval, '\\');
+}
 
-  NEXT(TokenKind::LIT_STR)
-  ASSERT_EQ(t->sval, "string");
+TEST(LexerTest, Valid_String) {
+  std::stringstream in;
+  in << "\"aaaaaa\" \"アイウエオ\"";
+  Lexer lexer(in);
+  std::unique_ptr<Token> t;
 
-  NEXT(TokenKind::KW_FN)
-  NEXT(TokenKind::KW_LET)
-  NEXT(TokenKind::KW_VAR)
-  NEXT(TokenKind::KW_RET)
-  NEXT(TokenKind::KW_EXT)
-  NEXT(TokenKind::KW_IF)
-  NEXT(TokenKind::KW_ELSE)
+  NEXT(TokenKind::LIT_STR);
+  ASSERT_EQ(t->sval, "aaaaaa");
+  NEXT(TokenKind::LIT_STR);
+  ASSERT_EQ(t->sval, "アイウエオ");
+}
 
-  NEXT(TokenKind::PLUS)
-  NEXT(TokenKind::MINUS)
-  NEXT(TokenKind::SLASH)
-  NEXT(TokenKind::STAR)
-  NEXT(TokenKind::PERCENT)
-  NEXT(TokenKind::AND)
-  NEXT(TokenKind::OR)
-  NEXT(TokenKind::CARET)
-  NEXT(TokenKind::SHL)
-  NEXT(TokenKind::SHR)
-  NEXT(TokenKind::ANDAND)
-  NEXT(TokenKind::OROR)
-  NEXT(TokenKind::LT)
-  NEXT(TokenKind::LE)
-  NEXT(TokenKind::GT)
-  NEXT(TokenKind::GE)
-  NEXT(TokenKind::EQEQ)
-  NEXT(TokenKind::NEQ)
-  NEXT(TokenKind::NOT)
-  NEXT(TokenKind::LPAREN)
-  NEXT(TokenKind::RPAREN)
-  NEXT(TokenKind::LBRACE)
-  NEXT(TokenKind::RBRACE)
-  NEXT(TokenKind::EQ)
-  NEXT(TokenKind::SEMI)
-  NEXT(TokenKind::COLON)
-  NEXT(TokenKind::COMMA)
-  NEXT(TokenKind::ARROW)
-  NEXT(TokenKind::END)
+TEST(LexerTest, Valid_KW) {
+  std::stringstream in;
+  in << "fn let var ret ext if else";
+  Lexer lexer(in);
+  std::unique_ptr<Token> t;
+
+  NEXT(TokenKind::KW_FN);
+  NEXT(TokenKind::KW_LET);
+  NEXT(TokenKind::KW_VAR);
+  NEXT(TokenKind::KW_RET);
+  NEXT(TokenKind::KW_EXT);
+  NEXT(TokenKind::KW_IF);
+  NEXT(TokenKind::KW_ELSE);
+}
+
+TEST(LexerTest, Valid_Symbol) {
+  std::stringstream in;
+  in << "+ - / * % & | ^ << >> && || < <= > >= == != !(){}=;:,-> /* comment \
+  */";
+  Lexer lexer(in);
+  std::unique_ptr<Token> t;
+
+  NEXT(TokenKind::PLUS);
+  NEXT(TokenKind::MINUS);
+  NEXT(TokenKind::SLASH);
+  NEXT(TokenKind::STAR);
+  NEXT(TokenKind::PERCENT);
+  NEXT(TokenKind::AND);
+  NEXT(TokenKind::OR);
+  NEXT(TokenKind::CARET);
+  NEXT(TokenKind::SHL);
+  NEXT(TokenKind::SHR);
+  NEXT(TokenKind::ANDAND);
+  NEXT(TokenKind::OROR);
+  NEXT(TokenKind::LT);
+  NEXT(TokenKind::LE);
+  NEXT(TokenKind::GT);
+  NEXT(TokenKind::GE);
+  NEXT(TokenKind::EQEQ);
+  NEXT(TokenKind::NEQ);
+  NEXT(TokenKind::NOT);
+  NEXT(TokenKind::LPAREN);
+  NEXT(TokenKind::RPAREN);
+  NEXT(TokenKind::LBRACE);
+  NEXT(TokenKind::RBRACE);
+  NEXT(TokenKind::EQ);
+  NEXT(TokenKind::SEMI);
+  NEXT(TokenKind::COLON);
+  NEXT(TokenKind::COMMA);
+  NEXT(TokenKind::ARROW);
+  NEXT(TokenKind::END);
 }
 
 }  // namespace felis
